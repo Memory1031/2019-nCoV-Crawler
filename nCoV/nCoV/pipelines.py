ad88521ff9c2eb585b1a43cfb859fa199055a54a
SoCMo <247805431@qq.com>
2020-02-07 01:42:59 +0800

    fix:update
    Signed-off-by: SoCMo <247805431@qq.com>

--- nCoV/nCoV/pipelines.py
+++ nCoV/nCoV/pipelines.py
@@ -6,7 +6,6 @@
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 import MySQLdb
 
-
 class NcovPipeline(object):
     def __init__(self):
         # 打开数据库连接
@@ -23,11 +22,12 @@ class NcovPipeline(object):
         latestUpdate = cursor.fetchone()[3]
         cursor.close()
 
-        if latestUpdate != getTime:
-            cursorThen = self.db.cursor()
-            cursorThen.execute(
+        if latestUpdate.__format__('%Y-%m-%d') != getTime:
+            cursorTime = self.db.cursor()
+            cursorTime.execute(
                 "UPDATE dataFromTencent_dev SET date = '%s' WHERE 'provinceName' = 'latestUpdate'" % (getTime))
             self.db.commit()
+            cursorTime.close()
 
         for province in item['areaTree']:
             provinceName = province['name']
@@ -46,11 +46,11 @@ class NcovPipeline(object):
 
                 try:
                     cursorThen = self.db.cursor()
-                    if latestUpdate == getTime:
+                    if latestUpdate.__format__('%Y-%m-%d') == getTime:
                         cursorThen.execute(
-                            "UPDATE dataFromTencent_dev SET totalConfirm = %d, totalSuspect = %d, totalDead = %d, totalHeal = %d, todayConfirm = %d, todaySuspect = %d, todayDead = %d, todayHeal = `%d` WHERE (cityName = '%s' and date = '%s')") \
-                        % (totalConfirm, totalSuspect, totalDead, totalHeal, todayConfirm, todaySuspect, todayDead,
-                           todayHeal, cityName, getTime)
+                            "UPDATE dataFromTencent_dev SET totalConfirm = %d, totalSuspect = %d, totalDead = %d, totalHeal = %d, todayConfirm = %d, todaySuspect = %d, todayDead = %d, todayHeal = %d WHERE cityName = '%s' and date = '%s'" \
+                            % (totalConfirm, totalSuspect, totalDead, totalHeal, todayConfirm, todaySuspect, todayDead,
+                               todayHeal, cityName, getTime))
                         self.db.commit()
                     else:
                         sql = "INSERT INTO dataFromTencent_dev (provinceName, cityName, date, totalConfirm, totalSuspect, totalDead, totalHeal, todayConfirm, todaySuspect, todayDead, todayHeal) VALUES ('%s', '%s', '%s', %d, %d, %d, %d, %d, %d, %d, %d)" \
@@ -58,6 +58,7 @@ class NcovPipeline(object):
                                  totalHeal, todayConfirm, todaySuspect, todayDead, todayHeal)
                         cursorThen.execute(sql)
                         self.db.commit()
+                        pass
                 except:
                     print("与数据库交互失败！")
                     self.db.rollback()
